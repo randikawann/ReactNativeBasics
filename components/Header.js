f21b2c1c5bddaadb98b7bb9e8ef3bf8e623e8c70
randika <randika@arimaclanka.com>
2022-03-15 11:25:21 +0530

    edit header text with react dom

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
-const Header = (props) => {
+const Header = ({title}) => {
   return(
     <View style = {styles.header}>
-      <Text style={styles.text}>{props.title}</Text>
+      <Text style={styles.text}>{title}</Text>
   
     </View>
   )
