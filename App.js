1fb7306d4a8b08daa53cf90bb761e92dae5cf615
randika <randika@arimaclanka.com>
2022-03-15 09:51:47 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+  return(
+    <View style = {{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{color: 'darkslateblue', fontSize: 30}}>Hellow</Text>
+    </View>
+  )
+}
+
+
+export default App;
