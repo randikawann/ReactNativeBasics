5a379be0933fc70dbbc27a0561ec385556a2323e
randika <randika@arimaclanka.com>
2022-03-15 11:21:34 +0530

    added header component

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import Header from './components/Header'
 
 const App = () => {
   return(
     <View style = {styles.container}>
-      <Text style={styles.text}>Hellow</Text>
-      <Image source={{uri: 'https://i.guim.co.uk/img/media/d1a8023b09fb6f46fafed7566e742e4eb5fc0c43/0_1889_3648_2189/master/3648.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=9f94239a31f81093d07216f42caf900e'}} style={styles.img}/>
-
-
+      <Header />
+  
     </View>
   )
 }
@@ -16,21 +15,8 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingTop:60,
   },
-
-  text: {
-    color: 'darkslateblue',
-    fontSize:30,
-  
-  },
-
-  img: {
-    width: 100,
-    height: 100,
-    borderRadius: 100/2,
-  }
 })
 
 export default App;
