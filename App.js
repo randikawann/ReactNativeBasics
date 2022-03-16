d32e5252621858b666e649c505a68d667eeb560a
randika <randika@arimaclanka.com>
2022-03-16 17:05:36 +0530

    initial branch commit

--- App.js
+++ App.js
@@ -1,25 +1,11 @@
-import React , {useState} from 'react';
-import {View, Text, StyleSheet, } from 'react-native';
+import React from 'react';
+import {View, Text, StyleSheet } from 'react-native';
 import Header from './components/Header';
-import 'react-native-get-random-values';
-import {uuid} from 'uuidv4';
-
 const App = () => {
-
-  const [items, setItems] = useState([
-    {
-      id: uuid(),
-      text: 'Milk',
-    },
-  ]);
 
   return(
     <View style = {styles.container}>
       <Header title='Shopping list'/>
-      <FlatList
-        data={items}
-        renderItem={({item}) => <Text>{item.text}</Text>}
-      />
     </View>
   )
 }
