967fbe76438c2f0ed49de483e66a474b86a345d3
randika <randika@arimaclanka.com>
2022-03-16 09:21:14 +0530

    error with uuid import

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import Header from './components/Header'
+import React , {useState} from 'react';
+import {View, Text, StyleSheet, } from 'react-native';
+import Header from './components/Header';
+import 'react-native-get-random-values';
+import {uuid} from 'uuidv4';
 
 const App = () => {
+
+  const [items, setItems] = useState([
+    {
+      id: uuid(),
+      text: 'Milk',
+    },
+  ]);
+
   return(
     <View style = {styles.container}>
       <Header title='Shopping list'/>
-  
+      <FlatList
+        data={items}
+        renderItem={({item}) => <Text>{item.text}</Text>}
+      />
     </View>
   )
 }
