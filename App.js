89d105bef253e1746827e139e71eb7d04ce01982
randika <randika@arimaclanka.com>
2022-03-16 17:45:06 +0530

    changed states by clicking button

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import {View, Text, StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Button } from 'react-native';
 import Header from './components/Header';
 const App = () => {
+
+  const [name, setName] = useState('Mash');
+
+  const changeStates = () => {
+    if(name == 'Mash'){
+      setName('Clicked button')
+    }else{
+      setName('Mash')
+    }
+  }
+
 
   return(
     <View style = {styles.container}>
       <Header title='Shopping list'/>
+
+      <Text style = {styles.bodytext}>This is text {name}</Text>
+      <Button title='Click here to changed button' onPress={changeStates}></Button>
     </View>
   )
 }
@@ -16,6 +30,9 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop:60,
   },
+  bodytext:{
+    color: 'red'
+  }
 })
 
 export default App;
