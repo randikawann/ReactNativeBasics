62b0fe4efb184646ebaf70fb97806ffa670236cf
randika <randika@arimaclanka.com>
2022-03-18 09:05:27 +0530

    list view onRefreshing function

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, Button } from 'react-native';
+import {View, Text, StyleSheet, ScrollView, RefreshControl } from 'react-native';
 import Header from './components/Header';
 const App = () => {
 
@@ -10,17 +10,42 @@ const App = () => {
     {key:4, item: 'item 4'},
     {key:5, item: 'item 5'},
     {key:6, item: 'item 6 '},
+    {key:7, item: 'item 6 '},
+    {key:8, item: 'item 6 '},
+    {key:9, item: 'item 6 '},
+    {key:10, item: 'item 6 '},
+    {key:11, item: 'item 6 '},
+    {key:12, item: 'item 6 '},
+    {key:13, item: 'item 6 '},
+    {key:14, item: 'item 6 '},
+    {key:15, item: 'item 6 '},
+    {key:16, item: 'item 6 '},
+    {key:17, item: 'item 6 '},
+    {key:18, item: 'item 6 '},
+
   ])
+
+  const [refreshing, setRefreshing] = useState(false)
+
+  const onRefresh = () => {
+    setRefreshing(true)
+    setItems([... items, {key:69, item: 'Item 69'}])
+    setRefreshing(false)
+  }
+
 
   return(
     <View style = {styles.container}>
-      {
-        items.map((i) => {return(
-          <View style={styles.item} key={i.key}>
-            <Text style= {styles.text}>{i.item}</Text>
-          </View>
-        )})
-      }
+      <ScrollView refreshControl={
+        <RefreshControl refreshing = {refreshing} onRefresh={onRefresh} />}>
+        {
+          items.map((i) => {return(
+            <View style={styles.item} key={i.key}>
+              <Text style= {styles.text}>{i.item}</Text>
+            </View>
+          )})
+        }
+      </ScrollView>
     </View>
   )
 }
