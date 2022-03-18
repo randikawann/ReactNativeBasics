6a7016d7a184a72e00756a78956fdcb752816a6a
randika <randika@arimaclanka.com>
2022-03-18 07:46:42 +0530

    responsive ui screen

--- App.js
+++ App.js
@@ -3,25 +3,22 @@ import {View, Text, StyleSheet, Button } from 'react-native';
 import Header from './components/Header';
 const App = () => {
 
-  const [incrementNumber, setIncrementNumber] = useState(0);
-  const [singleIncrement, setSingleIncrement] = useState(0);
-
-  const changeStates = () => {
-
-    setSingleIncrement(singleIncrement+1)
-    setIncrementNumber(incrementNumber+5)
-  
-  }
-
-
   return(
     <View style = {styles.container}>
-      <Header title='Shopping list'/>
-
-      <View style = {styles.contentContainer}>
-      <Text style = {styles.bodytext}>{incrementNumber}</Text>
-      <Button title='Increment' onPress={changeStates}></Button>
-      <Text style = {styles.bodytext}>You clicked {singleIncrement} times</Text>
+      <View style={styles.topContainer}>
+      <View style={styles.one}><Text style={styles.text}>1</Text></View>
+      <View style={styles.two}><Text style={styles.text}>2</Text></View>
+      <View style={styles.three}><Text style={styles.text}>3</Text></View>
+      </View>
+      <View style={styles.secondContainer}>
+        <Text style={styles.text}>4</Text>
+      </View>
+      <View style={styles.thirdContainer}>
+        <Text style={styles.text}>5</Text>
+      </View>
+      <View style={styles.fourthContainer}>
+        <View style={styles.six}><Text style={styles.text}>6</Text></View>
+      <View style={styles.seven}><Text style={styles.text}>7</Text></View>
       </View>
     </View>
   )
@@ -33,14 +30,61 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: 30,
   },
-  contentContainer:{
+  topContainer: {
     flex: 1,
-    alignItems: 'center',
-    marginTop: 30
+    flexDirection: 'row',
   },
-  bodytext:{
-    color: 'black'
-  }
+  one:{
+    justifyContent: 'center',
+    flex: 1,
+    backgroundColor: "lightblue"
+  },
+  two:{
+    justifyContent: 'center',
+    flex: 2,
+    backgroundColor: "purple"
+  },
+  three:{
+    justifyContent: 'center',
+    flex: 3,
+    backgroundColor: "yellow"
+  },
+  secondContainer: {
+    justifyContent: 'center',
+    flex: 1,
+    backgroundColor: 'red'
+  },
+  thirdContainer: {
+    justifyContent: 'center',
+    flex: 1,
+    backgroundColor: 'green'
+  },
+  fourthContainer: {
+    flexDirection: 'row',
+    flex: 5,
+    backgroundColor: 'green'
+  },
+
+  six: {
+    justifyContent: 'center',
+    flex: 1,
+    backgroundColor: "white"
+  },
+
+  seven: {
+    justifyContent: 'center',
+    flex: 1,
+    backgroundColor: "blue"
+  },
+
+  text:{
+    textAlign: 'center',
+    fontSize: 25,
+    fontWeight: 'bold',
+    fontStyle: 'italic'
+    
+  },
+
 })
 
 export default App;
