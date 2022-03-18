5f5aa8a01b57cd3c40ff90dc452aae7e20d06b1f
randika <randika@arimaclanka.com>
2022-03-18 08:47:26 +0530

    basic list view

--- App.js
+++ App.js
@@ -3,23 +3,24 @@ import {View, Text, StyleSheet, Button } from 'react-native';
 import Header from './components/Header';
 const App = () => {
 
+  const [items, setItems] = useState([
+    {key:1, item: 'item 1'},
+    {key:2, item: 'item 2'},
+    {key:3, item: 'item 3'},
+    {key:4, item: 'item 4'},
+    {key:5, item: 'item 5'},
+    {key:6, item: 'item 6 '},
+  ])
+
   return(
     <View style = {styles.container}>
-      <View style={styles.topContainer}>
-      <View style={styles.one}><Text style={styles.text}>1</Text></View>
-      <View style={styles.two}><Text style={styles.text}>2</Text></View>
-      <View style={styles.three}><Text style={styles.text}>3</Text></View>
-      </View>
-      <View style={styles.secondContainer}>
-        <Text style={styles.text}>4</Text>
-      </View>
-      <View style={styles.thirdContainer}>
-        <Text style={styles.text}>5</Text>
-      </View>
-      <View style={styles.fourthContainer}>
-        <View style={styles.six}><Text style={styles.text}>6</Text></View>
-      <View style={styles.seven}><Text style={styles.text}>7</Text></View>
-      </View>
+      {
+        items.map((i) => {return(
+          <View style={styles.item} key={i.key}>
+            <Text style= {styles.text}>{i.item}</Text>
+          </View>
+        )})
+      }
     </View>
   )
 }
@@ -30,60 +31,16 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: 30,
   },
-  topContainer: {
-    flex: 1,
-    flexDirection: 'row',
-  },
-  one:{
+  item: {
+    margin: 10,
+    backgroundColor: 'lightblue',
     justifyContent: 'center',
-    flex: 1,
-    backgroundColor: "lightblue"
+    alignItems: 'center'
   },
-  two:{
-    justifyContent: 'center',
-    flex: 2,
-    backgroundColor: "purple"
-  },
-  three:{
-    justifyContent: 'center',
-    flex: 3,
-    backgroundColor: "yellow"
-  },
-  secondContainer: {
-    justifyContent: 'center',
-    flex: 1,
-    backgroundColor: 'red'
-  },
-  thirdContainer: {
-    justifyContent: 'center',
-    flex: 1,
-    backgroundColor: 'green'
-  },
-  fourthContainer: {
-    flexDirection: 'row',
-    flex: 5,
-    backgroundColor: 'green'
-  },
-
-  six: {
-    justifyContent: 'center',
-    flex: 1,
-    backgroundColor: "white"
-  },
-
-  seven: {
-    justifyContent: 'center',
-    flex: 1,
-    backgroundColor: "blue"
-  },
-
-  text:{
-    textAlign: 'center',
-    fontSize: 25,
-    fontWeight: 'bold',
-    fontStyle: 'italic'
-    
-  },
+  text: {
+    color: 'black',
+    fontSize: 35,
+  }
 
 })
 
