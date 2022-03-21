e2a17d830ecc9ef7adca9257dc847ea55e1b4390
randika <randika@arimaclanka.com>
2022-03-21 07:30:58 +0530

    touchable opacity

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, FlatList, RefreshControl } from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native';
 import SectionList from 'react-native/Libraries/Lists/SectionList';
 import Header from './components/Header';
 const App = () => {
 
+  const onPressHandler = () => {
+    Alert.alert("Greatings", "Hi darling")
+  }
   return(
     <View style = {styles.container}>
-      
+      <TouchableOpacity style={styles.button} onPress={onPressHandler}>
+        <Text>Button</Text>
+      </TouchableOpacity>
     </View>
   )
 }
@@ -21,6 +26,16 @@ const styles = StyleSheet.create({
     color: 'black',
     fontSize: 35,
   },
+
+  button: {
+    marginTop: 100,
+    alignSelf: 'center',
+    backgroundColor: 'blue',
+    color: 'white',
+    width: 100,
+    height: 50,
+    alignItems: 'center'
+  }
 
 })
 
