b9907ae83a30643309d43b0c4501ac79f3e9f260
randika <randika@arimaclanka.com>
2022-03-22 08:52:24 +0530

    modal - custom modal design for popup

--- App.js
+++ App.js
@@ -1,19 +1,43 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, TextInput, Alert, ToastAndroid } from 'react-native';
+import {View, Text, StyleSheet, TextInput, Alert, ToastAndroid, Modal } from 'react-native';
 import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
 import SectionList from 'react-native/Libraries/Lists/SectionList';
 import Header from './components/Header';
 const App = () => {
 
   const onPressHandler = () => {
-    
+    setShowarning(true)
   }
 
   const [name, setName] = useState('')
+  const [showWarning, setShowarning] = useState(false);
 
   return(
     <View style = {styles.container}>
 
+      <Modal
+        visible = {showWarning}
+        onRequestClose = {() => {setShowarning(false)}}
+        transparent // this is for close in back button
+        animationType='fade'
+        hardwareAccelerated // this is for android for speed popup
+      >
+        <View style = {styles.warn_container}>
+          <View style = {styles.warning_modal}>
+            <View style = {styles.warning_title}>
+              <Text style={styles.warn_text}>Warning</Text>
+            </View>
+            <View style={styles.warning_body}>
+              <Text>Hi, this is modal popup</Text>
+            </View>
+            <Pressable
+              style={styles.warn_btn}
+              onPress={()=> setShowarning(false)}>
+              <Text style = {styles.warn_text}>OK</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
       <TextInput 
         style={styles.textInput}
         placeholder= {'eg. Randiksd'}
@@ -57,7 +81,47 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 50,
     margin: 20,
-  }
+  },
+  warning_modal:{
+    width: 300,
+    height: 300,
+    backgroundColor: '#ffffff',
+    borderColor: '#ffffff',
+    borderWidth: 1,
+    borderRadius: 10,
+  },
+  warn_container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignContent: 'center',
+    backgroundColor: '#00000099'
+  },
+  warning_title: {
+     height:50, 
+     justifyContent: 'center',
+     alignItems: 'center',
+     backgroundColor: '#ff0',
+     borderTopLeftRadius: 10,
+     borderTopRightRadius: 10,
+  },
+  warn_text : {
+    color: 'black',
+    fontSize: 35,
+    alignItems:'center'
+    
+  },
+  warning_body: {
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  warn_btn: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomLeftRadius: 10,
+     borderBottomRightRadius: 10,
+  },
 
 })
 
