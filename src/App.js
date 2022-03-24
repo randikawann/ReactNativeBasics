b2926ced0206add8d6518ccbc422c7d7e468a316
randika <randika@arimaclanka.com>
2022-03-24 14:01:48 +0530

    createStackNavigator - navigate screenA to screen B

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 
 import React, {useState} from 'react';
-import {View, Text } from 'react-native';
+import {View, Text , StyleSheet} from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
 
 const stack = createStackNavigator();
  
-function ScreenA(){
+function ScreenA({navigation}){
+
+  const onPressHandler = () => {
+    navigation.navigate('Screen_B')
+  }
+
   return(
-    <View>
+    <View style = {styles.body}>
       <Text>Screen A</Text>
+      <Pressable > 
+        <Text onPress={onPressHandler}>Click here</Text>
+      </Pressable>
     </View>
   )
 }
+
+function ScreenB(){
+  return(
+    <View style = {styles.body}>
+      <Text>Screen B</Text>
+    </View>
+  )
+}
+
 function App(){
 
   return(
@@ -23,10 +41,22 @@ function App(){
            name = "Screen_A"
            component={ScreenA}
         />
+        <stack.Screen 
+           name = "Screen_B"
+           component={ScreenB}
+        />
       </stack.Navigator>
 
     </NavigationContainer>
   )
 }
+
+const styles = StyleSheet.create({
+  body:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
 export default App;
