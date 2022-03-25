28d8dff128d5c3b0c130fcfa69d9d7a69708f39c
randika <randika@arimaclanka.com>
2022-03-25 16:49:22 +0530

    added tab nativation icons

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import ScreenA from './ScreenA';
 import ScreenB from './ScreenB';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome'
 
 const Tab = createBottomTabNavigator();
 
@@ -14,7 +16,27 @@ function App(){
 
   return(
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, size, color}) => {
+            let iconName;
+            if(route.name == 'Screen_A'){
+                // <i class="far fa-acorn"></i>
+              iconName = 'openid';
+            }else if(route.name == 'Screen_B'){
+              iconName = 'star';
+            }
+
+            return (
+              <FontAwesomeIcon 
+                name= {iconName}
+                size = {25}
+                color = 'blue'
+              />
+            )
+          }
+        })}
+      >
         <Tab.Screen 
            name = "Screen_A"
            component={ScreenA}
